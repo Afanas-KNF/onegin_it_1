b748cebe2b1e051866ebac6626755463660a0849
Afanasy <hyhafanasy@gmail.com>
2024-09-10 17:17:10 -0700

    onegin
    переносим на линукс

--- onegin.cpp
+++ onegin.cpp
@@ -1,26 +1,52 @@
 #include <stdio.h>
-const int maxlen = 7;
-const int numstr = 6;
+#include <ctype.h> //чтобы пользоваться isalpha()
+//#include "onegin.txt"
+const int maxlen = 50;// максимальная длина строки
+const int numstr = 13;  // количетсов строк
+const int numsimb = 1000; // количество символов скопированных из файла
 
-void sort_str(char [][maxlen]); // char(*)[maxlen] это плюсовая запись, опка не осознал как записывать на си
+void sort_str(char [][maxlen]); // char(*)[maxlen] что означает эта запись?
 bool str_is_bigger(char*, char*);
 void swap_str(char*, char*);
 void print_text(char [][maxlen]);
+void fill_text(char [],char [][maxlen]);
 
 
 int main()
 {
-    char text[numstr][maxlen] = {{"abcd"},{"efghi"},{"j"},{"kl"},{"mno"},{"qrstuv"}};
-    printf("%s",text[1]);
-    printf("\n%c",text[1][3]);
-    printf("\n%d",text[1][3]);
-    printf("\n%d",*text[1]+3);
-    printf("\n%d",text);       // берем адрес первой строки
-    printf("\n%d",text +1);    // берем адрес второй строки
-    printf("\n%d",text[0] +1); //берем адрес первой строки 2 эллемента
+    FILE *file;
+    file = fopen("onegin.txt", "r"); //открываем файл для чтения
+    if (file == NULL)               // проверка того смогли ли мы открыть файл
+    {
+      printf("Error: Failed to open file\n");
+      return 1;
+    }
+    char copytxt[numsimb] = {};       // в этот массив будем копировать текст из файла onegin.txt
+    int count = 0;
+    for(int i = 0, c; (c = fgetc(file)) != EOF; i++)// будем записывать с помощью функции fgetc посимвольно пока не встртим EndOfFIle
+    {                                               // символы \n тоже записываются
+        count++;
+        copytxt[i] = c;
+    }
+    printf("%d\n", count);  // выводит количество символов записанных в массив
+    for(int i = 0; i < numsimb; i++)
+    {
+        printf("%c", copytxt[i]); //выводит то что скопировали из текста
+    }
+
+    //fgets(copytxt, numsimb, file);   // будет читать только одну строку так как функция считывает до первого символа \n
+
+    char text[][maxlen] = {};   // в этот массив будем записывать все алфавитные символы и пробелы а если встречается \n
+    fill_text(copytxt, text);
     sort_str(text);
     print_text(text);
-
+    // printf("%s",text[1]);
+    // printf("\n%c", text[1][3]);
+    // printf("\n%d", text[1][3]);
+    // printf("\n%d", *text[1]+3);
+    // printf("\n%d", text);         // берем адрес первой строки
+    // printf("\n%d", text +1);      // берем адрес второй строки
+    // printf("\n%d", text[0] +1);   //берем адрес первой строки 2 эллемента
 }
 
 void sort_str(char text[][maxlen])
@@ -81,12 +107,26 @@ void print_text(char text[][maxlen])
     }
 }
 
+void fill_text(char copytxt[], char text[][maxlen])
+{
+    for (int i = 0; i < numstr; i++)
+    {
+        int a = 0;
+        for (int j = 0; j  < maxlen; j++)
+        {
 
-
-
-
-
-
-
-
-
+            if (isalpha(copytxt[i*maxlen + j]) || copytxt[i*maxlen + j] == ' ')
+            {
+                text[i][a] = copytxt[i*maxlen + j];
+                printf("%c", text[i][a]);
+                a++;
+            }
+            else if (copytxt[i*maxlen + j] == '\n')
+            {
+                printf("%c", text[i][a]);
+                break;
+            }
+        }
+    }
+    // print_text(text);
+}
